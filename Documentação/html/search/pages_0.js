78a089fcef5ac366866184f8023cc4e6aefc6a69
ThiagoAndrew <51100438+ThiagoAndrew@users.noreply.github.com>
2019-11-25 16:28:53 -0300

    Add files via upload

---
+++ Documentação/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['projeto_2ditp_3',['Projeto-ITP',['../md__projeto-_i_t_p-master__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ Documentação/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "pr",
+  1: "r",
+  2: "p"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Arquivos",
+  2: "Páginas"
+};
+
