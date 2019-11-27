f84b75984822e05694780b3d54ba93ab23950c7b
ThiagoAndrew <51100438+ThiagoAndrew@users.noreply.github.com>
2019-11-27 18:25:59 -0300

    Update StructsIniciais.h

--- StructsIniciais.h
+++ StructsIniciais.h
@@ -7,7 +7,7 @@ typedef struct entrada_t
 {
     char nome_entrada[30]; /*!< nome da primitiva. */  
     int qtdentrada; /*!< quantidade de entrada passados na primitiva. */  
-    int entrada[30]; /*!< entrada da primitiva. */  
+    int entrance[30]; /*!< entrada da primitiva. */  
     char entradastring[30]; /*!< Parâmetros em String da primitiva*/  
     
 }entrada;
