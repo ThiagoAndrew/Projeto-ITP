cbf6fa62a6907967d81a0d9dc281f982aecc6288
ThiagoAndrew <51100438+ThiagoAndrew@users.noreply.github.com>
2019-11-27 12:26:08 -0300

    Update and rename funcs.h to Funcs.h

--- Funcs.h
+++ Funcs.h
@@ -1,7 +1,7 @@
 /*! \file */ 
 
-#ifndef funcoes_h 
-#define funcoes_h
+#ifndef Funcs_h 
+#define Funcs_h
 
 //! Reparte uma string.
 /*!
@@ -10,7 +10,7 @@
 	\param delim recebe a string utilizada para quebrar a string em substrings.
 	\param retorno recebe um vetor de string que é usado para colocar a substrings geradas
 */
-//string_entrada == str_in
+//str_in entrada
 void split(char str_in[], char delim[], char retorno[][50]);
 
 //! Limpa strings.
@@ -19,7 +19,7 @@ void split(char str_in[], char delim[], char retorno[][50]);
 	\param entrada recebe a string a ser limpa
 	\param saida recebe uma string para depositar a string após ser limpa
 */
-//limpar_string_arquivo == clean_str_arq
+//clean_str_arq limpa a string
 void clean_str_arq(char entrada[], char saida[]);
 
 //! Retira a quebra de linha de uma string.
@@ -27,7 +27,7 @@ void clean_str_arq(char entrada[], char saida[]);
 	\param entrada recebe a string para ser retirada a quebra de linha.
 	\param saida recebe a string para ser colocada a string limpa.
 */
-//retira_barraN == remove_barN
+//remove_barN remove o /n.
 void remove_barN(char entrada[], char saida[]);
 
 //! Aloca a matriz da imagem
@@ -35,8 +35,8 @@ void remove_barN(char entrada[], char saida[]);
 	Faz a alocação inicial do vetor tridimensional que guarda os pixels referentes a imagem que está sendo editada.
 	\return Uma instancia da struct imagem com a matriz tridimensional alocada.
 */
-//alocar_matriz == aloc_mat
-imagem aloc_mat();
+//aloc_mat alocação da matriz
+image aloc_mat();
 
 //! Realoca a matriz de pixels com o tamanho necessário.
 /*!
@@ -44,8 +44,8 @@ imagem aloc_mat();
 	de bytes de bytes necessária para armazenar todos os pixels da imagem com tal resolução.
 	\param img recebe o ponteiro com a imagem a ser realocada.
 */
-//realocar_matriz == realoc_mat
-void realoc_mat(imagem *img);
+//realoc_mat realoca a matriz.
+void realoc_mat(image *img);
 
 //! Desaloca a matriz de pixels.
 /*!
@@ -53,7 +53,7 @@ void realoc_mat(imagem *img);
 	foram alocados na matriz de acordo com essa resolução.
 	\param img recebe o ponteiro com a imagem a ser desalocada.
 */
-//desalocar_matriz == desaloc_mat
-void desaloc_mat(imagem *img);
+//desaloc_mat desaloca a matriz.
+void desaloc_mat(image *img);
 
 #endifv
